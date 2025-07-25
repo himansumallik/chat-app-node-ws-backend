1fc0a73323582deeb3f26e4a023e69966a565b07
Himansu Mallik <dreamhimansu@gmail.com>
2025-07-25 13:26:27 +0530

    setting up a server

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
+import express from 'express'
 import http from 'node:http'
-import ws from 'ws'
+import { Server } from 'socket.io';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
 
-const PORT = 8000
+const PORT = 4000
 
-const server = http.createServer(async (req,res)=>{
+const app= express();
+const server = http.createServer(app)
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get('/', (req,res)=>{
     res.setHeader('Content-Type','application/json')
     res.statusCode = 200
     res.end("Hello from the ws server")
 })
+
+// const server = http.createServer(async (req,res)=>{
+//     res.setHeader('Content-Type','application/json')
+//     res.statusCode = 200
+//     res.end("Hello from the ws server")
+// })
 
 server.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`)
